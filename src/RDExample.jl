79c5da16f6d81791bcce9d8d55f4d46ac772e10e
Raj <rajd@mit.edu>
2020-05-06 14:34:35 -0400

    Run tests

--- src/RDExample.jl
+++ src/RDExample.jl
@@ -2,4 +2,8 @@ module RDExample
 
 greet() = print("Hello World!")
 
+include("extrafile_RD.jl")
+
+export my_fun
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
+
 using RDExample
 using Test
 
+
 @testset "RDExample.jl" begin
+    @test my_fun(2, 2) == 10
+    @test my_fun(2, 3) == 13
     # Write your own tests here.
 end
